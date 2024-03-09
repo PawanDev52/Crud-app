a8fb0eca70c4c8316e4ede0334499703a3deb541
PawanDev52 <133631214+PawanDev52@users.noreply.github.com>
2024-03-09 15:50:48 +0530

    php file

--- 1index.php
+++ 1index.php
@@ -95,7 +95,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   </div>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="/crud_app/php.png" height="40px" alt=""></a>
+      <a class="navbar-brand" href="1index.php"><img src="/crud_app/php.png" height="40px" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
